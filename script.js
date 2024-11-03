fe738f4a70350c95685f23b3e61b474e88cf3e4f
vinod <vinodkumar@vinods-MacBook-Air.local>
2024-11-03 18:24:58 +0530

    deploy

---
+++ script.js
@@ -0,0 +1,14 @@
+let lastScrollTop = 0;
+
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        navbar.classList.add('hidden');
+    } else {
+        navbar.classList.remove('hidden');
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+});
